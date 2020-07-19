a7d8033ea50f2c2065d88e21d352b4246e688bfb
fpemba <fatuma.pemba@yahoo.com>
2020-07-19 13:04:42 -0400

    added Chuck configuration

---
+++ src/main/java/com/pemba/jokeapp/controllers/JokeController.java
@@ -0,0 +1,25 @@
+package com.pemba.jokeapp.controllers;
+
+
+import com.pemba.jokeapp.services.JokeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class JokeController {
+
+    private JokeService jokeService;
+
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @GetMapping({"/", ""})
+    public String showJoke(Model model) {
+
+        model.addAttribute("joke",jokeService.getJoke());
+
+        return "chucknorris";
+    }
+}
